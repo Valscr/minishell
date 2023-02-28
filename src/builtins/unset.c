f0169de516af3ab61ca16439762e5791f7f52375
Loliloly <krozer93@hotmail.fr>
2023-02-28 22:32:20 +0100

    unset v4

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -3,35 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   unset.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: valentin <valentin@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 19:48:13 by valentin          #+#    #+#             */
-/*   Updated: 2023/02/28 21:33:30 by valentin         ###   ########.fr       */
+/*   Updated: 2023/02/28 22:31:27 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-int	ft_unset(char *name, t_data *data)
+int ft_unset(char *name, t_env *env)
 {
 	int		len;
 	t_env	*cur;
+	t_env	*tmp;
 
 	len = ft_strlen(name);
-	cur = data->env;
-	if (ft_strncmp(cur->value, name, len) == 0)
-	{
-		data->env = cur->next;
-		free(cur->value);
-		free(cur);
-	}
-	cur = data->env;
+	cur = env;
 	while (cur->next)
 	{
 		if (ft_strncmp(cur->next->value, name, len) == 0)
 		{
 			free(cur->next->value);
+			cur->next->value = NULL;
+			tmp = cur->next;
 			cur->next = cur->next->next;
+			free(tmp);
+			return (0);
 		}
 		cur = cur->next;
 	}
