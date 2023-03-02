a18569e2881c61748cafad2d26cb517fb2262b75
Loliloly <krozer93@hotmail.fr>
2023-03-02 15:41:13 +0100

    export a régler

--- src/exec/exec_builtins.c
+++ src/exec/exec_builtins.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/23 00:16:58 by valentin          #+#    #+#             */
-/*   Updated: 2023/03/02 00:01:20 by marvin           ###   ########.fr       */
+/*   Updated: 2023/03/02 15:33:55 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,6 +50,8 @@ int	check_arg2(char *str, t_data *data)
 	strg = NULL;
 	if (!ft_strncmp("cd", str, 2))
 	{
+		if (str[2] != ' ' && str[2] != '\0')
+			return (write_error("Command not found\n"), 0);
 		strg = ft_split(str, " '\"");
 		g_sig.code_error = ft_cd(strg, data->env);
 		return (free_tab_str(strg), 0);
@@ -57,13 +59,15 @@ int	check_arg2(char *str, t_data *data)
 	if (!ft_strncmp("export ", str, 7))
 	{
 		dest = ft_split(str, " ");
-		ft_export(dest[1], data);
+		if (dest[1])
+			ft_export(dest[1], data);
 		return (free_tab_str(dest), 0);
 	}
 	if (!ft_strncmp("unset ", str, 6))
 	{
 		dest = ft_split(str, " ");
-		g_sig.code_error = ft_unset(dest[1], data->env);
+		if (dest[1])
+			g_sig.code_error = ft_unset(dest[1], data->env);
 		return (free_tab_str(dest), 0);
 	}
 	free_tab_str(dest);
